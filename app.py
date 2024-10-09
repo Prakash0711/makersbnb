ea9a49936d3b3016b3116a112d3da0b9acc3c27d
Prakash Vijayanath <vijayanp@tcd.ie>
2024-10-09 15:45:47 +0100

    Minor changes to app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import os
-from flask import Flask, request, render_template, redirect, flash, jsonify, url_for, session
+from flask import (
+    Flask,
+    request,
+    render_template,
+    redirect,
+    flash,
+    jsonify,
+    url_for,
+    session,
+)
 from lib.database_connection import get_flask_database_connection
 from lib.space import Space
 from lib.space_repository import SpaceRepository
@@ -7,17 +16,20 @@ from lib.user_repository import UserRepository
 from lib.user import User
 from lib.booking import Booking
 from lib.booking_repository import BookingRepository
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+bcrypt = Bcrypt(app)
 
-@app.route('/home', methods=['GET'])
+
+@app.route("/home", methods=["GET"])
 def get_home():
     connection = get_flask_database_connection(app)
     repository = SpaceRepository(connection)
     space_list = repository.all()
-    return render_template('home.html', spaces = space_list)
+    return render_template("home.html", spaces=space_list)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+
+if __name__ == "__main__":
+    app.run(debug=True, port=int(os.environ.get("PORT", 5001)))
